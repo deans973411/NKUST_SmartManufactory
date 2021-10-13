ef53f26813a6801a7818be55e39a1f547c3d4f9f
Dean <74861204+deans973411@users.noreply.github.com>
2021-10-14 01:05:37 +0800

    Update Revise HW.py
    Transfor to EN

--- Revise HW.py
+++ Revise HW.py
@@ -1,20 +1,20 @@
-#班級:四模四丙 學號:C107147319 姓名:林鼎鈞
-print('班級:四模四丙 學號:C107147319 姓名:林鼎鈞')
+#Class:MDE4-3 Name:Dean
+print('Class:MDE4-3 Name:Dean')
 '''
-以while搭配每次迴圈+1個星號的函數，完成10次星號輸出
+Print stars shape 10 times with While command, the value which multplied plus 1 every loop
 '''
-print('【hsue HW1 星型數列】') 
-print('→以while搭配每次迴圈+1個星號的函數，完成10次星號輸出:\n') 
+print('【hsue HW1 Star shape sequence】') 
+print('→Print stars shape 10 times with While command, the value which multplied plus 1 every loop:\n') 
 
-i1 = 0   #定義初始值i(initial)
-t1 = 10  #定義目標值t(target)
+i1 = 0   #Define initial value i1
+t1 = 10  #Define target valie t1
 while i1 <= t1:
     i1 += 1
     print(i1*'*')
 
-#hsue HW2 最短九九乘法表(170字元)
-print('【hsue HW2 最短九九乘法表(170字元)】') 
-print('→沒啥好說的...:\n') 
+#hsue HW2 Shortest 9x9 multplication table(170chr)
+print('【hsue HW2 Shortest 9x9 multplication table(170chr)】') 
+print('→Nothing to say...:\n') 
 y = 1
 while y < 10:
     y += 1
@@ -25,20 +25,19 @@ while y < 10:
     
    
 
-#hsue HW2-1 可變九九乘法表
+#hsue HW2-1 Varibale multplication table
 '''
-以變數定義橫坐標初值與終值，並以while搭配每次迴圈針對縱坐標變數調整初值與終值
-，製作出可變乘法表
+The variable define initial value and final value, list it down with While command.
 '''
-print('【hsue HW2-1 可變九九乘法表】') 
-print('→以變數定義橫坐標初值與終值，並以while搭配每次迴圈針對縱坐標變數調整初值與終值，製作出可變乘法表:\n') 
+print('【hsue HW2-1 Varibale multplication table】') 
+print('→The variable define initial value and final value, list it down with While command.:\n') 
 
-[xi2, xt2] = [3, 18] #定義Y軸縱坐標初始值yi(initial)與極限值yt(terminal)
-[yi2, yt2] = [1, 18] #定義X軸橫坐標初始值xi(initial)與極限值xt(terminal)
-while yi2 <= yt2:
+[xi2, xf2] = [3, 18] #Define x-axis abscissa initial value xi and final value xf
+[yi2, yf2] = [1, 18] #Define y-axis ordinate initial value yi and final value yf
+while yi2 <= yf2:
     yi2 += 1
     for y2 in range(yi2-1, yi2):
-       for x2 in range(xi2, xt2+1):
+       for x2 in range(xi2, xf2+1):
             print('{:2}x{:2}={:4} ▍'.format(x2, y2, y2*x2), end="")
     print('\n')
 
